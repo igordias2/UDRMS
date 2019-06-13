9e5562df6396acb9873e429dbfb5fff1eb99047a
igordias2 <igordias.jd@gmail.com>
2019-06-12 21:33:44 -0300

    1° Commit: -Update de todos os Arquivos Necessários para o Projeto.

---
+++ UltimateDarkRiftMasterServer_UDRMS/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UltimateDarkRiftMasterServer_UDRMS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
